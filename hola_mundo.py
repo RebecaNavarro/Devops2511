1dc39060993a7879be424df60d4e6865570849c7
RebecaNavarro <130471557+RebecaNavarro@users.noreply.github.com>
2024-11-25 08:59:19 -0400

    Update hola_mundo.py

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,1 +1,9 @@
-print ("Hola Mundo, Este es mi primer pipeline")
+import os
+
+def main():
+    nombre = os.getenv("USERNAME")
+    print(f"¡Hola, {nombre} desde GitHub!")
+
+if __name__ == "__main__":
+    main()
+
